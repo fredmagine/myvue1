cb21e882d747d660e4219d32674d91159e63b742
fredmagine <fredmagine@gmail.com>
2017-08-07 10:18:18 +0800

    configure environment

---
+++ src/components/MyList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="myList">
+    <h1>{{ msg }}</h1>
+    <h2>Show my list</h2>
+    <button v-on:click="showAjaxList()">查询</button>
+    <table>
+        <tr>
+            <th>序号</th>
+            <th>姓名</th>
+             <th>密码</th>
+        </tr>
+        <tr v-for="item in myList">
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.password}}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myList',
+  data () {
+    return {
+      myList: [{id: 1, name: 'admin'}, {id: 2, name: 'hello'}],
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+      showAjaxList: function() {
+          let _self = this
+          this.post('http://127.0.0.1:9091/user/listAll', null, function(data) {
+            _self.myList = data
+          })
+        //   alert('Ajax requesting...')
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,23 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.prototype.post = function (url,params,fn) {
+  var xhr = new XMLHttpRequest()
+  xhr.open("POST", url, true)
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    var XMLHttpReq = xhr;
+    if (XMLHttpReq.readyState == 4) {
+      if (XMLHttpReq.status == 200) {
+        var text = XMLHttpReq.responseText
+        var res = JSON.parse(text)
+        fn(res)
+      }
+    }
+  }
+  xhr.send(params)
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
